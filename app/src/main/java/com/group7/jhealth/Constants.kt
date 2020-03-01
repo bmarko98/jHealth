aaa386a26391e3da9d708f8c454f43238176d590
Erman <erman.ergoz@hotmail.com>
2020-03-01 16:04:38 +0100

    started to implement FunctionalityFragment and PersonalInfoFragment

--- app/src/main/java/com/group7/jhealth/Constants.kt
+++ app/src/main/java/com/group7/jhealth/Constants.kt
@@ -1,2 +1,4 @@
 package com.group7.jhealth
 
+const val SHARED_PREF_FILE: String = "com.group7.jhealth"
+const val KEY_PREF_IS_FIRST_LAUNCH: String = "isFirstLaunch"
--- app/src/main/java/com/group7/jhealth/fragments/FunctionalityFragment.kt
+++ app/src/main/java/com/group7/jhealth/fragments/FunctionalityFragment.kt
@@ -1,2 +1,15 @@
 package com.group7.jhealth.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.group7.jhealth.R
+
+class FunctionalityFragment() : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_functionality, container, false)
+    }
+
+}
