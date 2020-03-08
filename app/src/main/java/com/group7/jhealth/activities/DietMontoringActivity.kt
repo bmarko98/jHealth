0346f7154568c7eccdea42f6716a28d39477693c
Diar Bojaxhi <diarbojaxhi@Diars-Macbook-Pro.local>
2020-03-08 19:11:39 +0100

    add Diet Monitoring Activity

---
+++ app/src/main/java/com/group7/jhealth/activities/DietMontoringActivity.kt
@@ -0,0 +1,13 @@
+package com.group7.jhealth.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.group7.jhealth.R
+
+class DietMontoringActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diet_montoring)
+    }
+}
--- app/src/main/java/com/group7/jhealth/fragments/FunctionalityFragment.kt
+++ app/src/main/java/com/group7/jhealth/fragments/FunctionalityFragment.kt
@@ -1,15 +1,25 @@
 package com.group7.jhealth.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.group7.jhealth.R
+import com.group7.jhealth.activities.DietMontoringActivity
+import kotlinx.android.synthetic.main.fragment_functionality.*
 
 class FunctionalityFragment() : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_functionality, container, false)
-    }
 
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        dietMonitoring.setOnClickListener{
+            startActivity(Intent(context,DietMontoringActivity::class.java))
+    }
+    }
 }
