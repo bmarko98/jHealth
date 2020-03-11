432c696c351df2ae4733aa2e5e0ed62bda6170e4
Yusuf Erman ERGÖZ <erman.ergoz@hotmail.com>
2020-03-11 04:29:28 +0100

    populate WaterTrackerActivity

--- app/src/main/java/com/group7/jhealth/Constants.kt
+++ app/src/main/java/com/group7/jhealth/Constants.kt
@@ -2,3 +2,5 @@ package com.group7.jhealth
 
 const val SHARED_PREF_FILE: String = "com.group7.jhealth"
 const val KEY_PREF_IS_FIRST_LAUNCH: String = "isFirstLaunch"
+
+const val WATER_INTAKE_TARGET_BAR_HEIGHT: Float = 5f
--- app/src/main/java/com/group7/jhealth/activities/WaterTrackerActivity.kt
+++ app/src/main/java/com/group7/jhealth/activities/WaterTrackerActivity.kt
@@ -3,18 +3,37 @@ package com.group7.jhealth.activities
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.core.content.ContextCompat
 import com.group7.jhealth.R
+import com.group7.jhealth.*
+import kotlinx.android.synthetic.main.activity_water_intake.*
 
 class WaterTrackerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.WaterIntakeTheme)
         setContentView(R.layout.activity_water_intake)
-
-        supportActionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(applicationContext,
-            R.color.darkBlue)))
-
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(applicationContext, R.color.darkBlue)))
         supportActionBar?.setTitle(R.string.water_tracker)
+
+        waterIntakeTargetBar.scaleY = WATER_INTAKE_TARGET_BAR_HEIGHT
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.water_tracker_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.drinkLog -> Log.e("menu", "drink log")
+            R.id.drinkReport -> Log.e("menu", "drink report")
+            R.id.waterTrackerSettings -> Log.e("menu", "settings")
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
