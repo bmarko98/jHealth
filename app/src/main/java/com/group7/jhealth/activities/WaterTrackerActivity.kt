e7fb935720fb45deec8bfc5c6210df4f4803a879
Yusuf Erman ERGÖZ <erman.ergoz@hotmail.com>
2020-03-11 03:21:39 +0100

    add colors and title for WaterTrackerActivity

--- app/src/main/java/com/group7/jhealth/activities/WaterTrackerActivity.kt
+++ app/src/main/java/com/group7/jhealth/activities/WaterTrackerActivity.kt
@@ -1,9 +1,9 @@
 package com.group7.jhealth.activities
 
-import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.group7.jhealth.R
 
 class WaterTrackerActivity : AppCompatActivity() {
@@ -11,5 +11,10 @@ class WaterTrackerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_water_intake)
+
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(applicationContext,
+            R.color.darkBlue)))
+
+        supportActionBar?.setTitle(R.string.water_tracker)
     }
 }
