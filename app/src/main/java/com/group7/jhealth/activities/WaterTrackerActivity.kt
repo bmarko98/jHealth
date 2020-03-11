8c86a93e7a3fc58ac20bef7be7bd8f110acf2ce0
Yusuf Erman ERGÖZ <erman.ergoz@hotmail.com>
2020-03-11 02:51:22 +0100

    add WaterTrackerActivity to activities

---
+++ app/src/main/java/com/group7/jhealth/activities/WaterTrackerActivity.kt
@@ -0,0 +1,15 @@
+package com.group7.jhealth.activities
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.group7.jhealth.R
+
+class WaterTrackerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_water_intake)
+    }
+}
--- app/src/main/java/com/group7/jhealth/fragments/FunctionalityFragment.kt
+++ app/src/main/java/com/group7/jhealth/fragments/FunctionalityFragment.kt
@@ -8,26 +8,32 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.group7.jhealth.R
 import com.group7.jhealth.activities.DietMonitoring
+import com.group7.jhealth.activities.WaterTrackerActivity
 import com.group7.jhealth.activities.WorkoutPlan
 import kotlinx.android.synthetic.main.fragment_functionality.*
 
 class FunctionalityFragment() : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_functionality, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        workoutButton.setOnClickListener{
+        workoutButton.setOnClickListener {
             startActivity(Intent(context, WorkoutPlan::class.java))
-
         }
 
-        dietMonitoring.setOnClickListener{
-            startActivity(Intent(context,DietMonitoring::class.java))
-
+        dietMonitoring.setOnClickListener {
+            startActivity(Intent(context, DietMonitoring::class.java))
         }
 
-}
+        waterTrackerButton.setOnClickListener {
+            startActivity(Intent(context, WaterTrackerActivity::class.java))
+        }
+    }
 }
