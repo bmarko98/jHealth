21c42e95492af82115b52852c415d811867a8c8f
arsara <arsovasara666@gmail.com>
2020-04-19 19:43:01 +0200

    Add imports and function to Record Entries on Database

--- app/src/main/java/com/group7/jhealth/fragments/SleepMonitoringFragment.kt
+++ app/src/main/java/com/group7/jhealth/fragments/SleepMonitoringFragment.kt
@@ -1,14 +1,31 @@
 package com.group7.jhealth.fragments
 
+//import kotlinx.android.synthetic.main.fragment_sleep_monitoring
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+//import androidx.navigation.fragment.findNavController
 import com.group7.jhealth.R
+import io.realm.Realm
 
 class SleepMonitoringFragment : Fragment() {
+
+    private lateinit var realm: Realm
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_sleep_monitoring, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        realm = Realm.getDefaultInstance()
+
+/*        .setOnClickListener {
+            findNavController().navigate(R.id.action_global_navigate_to_record_entry_fragment)
+        }*/
     }
 }
