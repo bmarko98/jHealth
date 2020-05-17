20d403233902c95955623e5d56fc5e35e14c5615
ermanergoz <ermanergoz@gmail.com>
2020-05-17 23:31:28 +0300

    Make UserInfo implement Parcelable in order to use Bundle

--- app/src/main/java/com/group7/jhealth/database/UserInfo.kt
+++ app/src/main/java/com/group7/jhealth/database/UserInfo.kt
@@ -1,5 +1,7 @@
 package com.group7.jhealth.database
 
+import android.os.Parcel
+import android.os.Parcelable
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 import java.util.*
@@ -10,14 +12,48 @@ import java.util.*
  * variables are initialized
  */
 
-open class UserInfo(
-    @PrimaryKey var id: Int = 0,
-    var name: String = "",
-    var age: Int = 0,
-    var gender: String = "",
-    var weight: Int = 0,
-    var wakeUpTime: Date? = Date(),
-    var sleepTime: Date? = Date(),
-    var drinkCupSize: Int = 250,
+open class UserInfo() : RealmObject(), Parcelable{
+    @PrimaryKey var id: Int = 0
+    var name: String = ""
+    var age: Int = 0
+    var gender: String = ""
+    var weight: Int = 0
+    var wakeUpTime: Date? = Date()
+    var sleepTime: Date? = Date()
+    var drinkCupSize: Int = 250
     var workoutDuration: Int = 0
-) : RealmObject()
+
+    constructor(parcel: Parcel) : this() {
+        id = parcel.readInt()
+        name = parcel.readString().toString()
+        age = parcel.readInt()
+        gender = parcel.readString().toString()
+        weight = parcel.readInt()
+        drinkCupSize = parcel.readInt()
+        workoutDuration = parcel.readInt()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(name)
+        parcel.writeInt(age)
+        parcel.writeString(gender)
+        parcel.writeInt(weight)
+        parcel.writeInt(drinkCupSize)
+        parcel.writeInt(workoutDuration)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<UserInfo> {
+        override fun createFromParcel(parcel: Parcel): UserInfo {
+            return UserInfo(parcel)
+        }
+
+        override fun newArray(size: Int): Array<UserInfo?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
