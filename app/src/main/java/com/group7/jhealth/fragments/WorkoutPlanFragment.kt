a0d09834beb01711fd717baf69a39277cf972ce3
ermanergoz <ermanergoz@gmail.com>
2020-05-19 22:28:59 +0300

    Populate ABSFragment

--- app/src/main/java/com/group7/jhealth/fragments/WorkoutPlanFragment.kt
+++ app/src/main/java/com/group7/jhealth/fragments/WorkoutPlanFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.group7.jhealth.R
+import kotlinx.android.synthetic.main.fragment_workout_plan.*
 
 /**
  * Class for Setting up the Workout Planning option
@@ -24,5 +27,15 @@ class WorkoutPlanFragment : Fragment() {
      */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_workout_plan, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+
+        ABSButton.setOnClickListener {
+            navController.navigate(R.id.action_nav_workout_plan_to_nav_abs_frag)
+        }
     }
 }
